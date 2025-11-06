6a1b8e42eb1840a9366987324ebfeacbea5973be
Nate McIntosh <natemcintosh@gmail.com>
2025-11-06 12:46:24 -0500

    now track cash earned

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use chrono::Local;
 
 /// Holds the state for a game
 #[derive(Debug, Clone, Copy)]
-struct Game {
+pub struct Game {
     /// The probability of flipping heads
     p_heads: f64,
 
@@ -24,6 +24,15 @@ struct Game {
 
     /// How many heads in a row? You win if you reach
     n_heads_in_a_row: usize,
+
+    /// Current value of the coin
+    coin_val: f64,
+
+    /// Current score multiplier
+    multiplier: f64,
+
+    /// Current cash
+    cash: f64,
 }
 
 impl Game {
@@ -35,6 +44,9 @@ impl Game {
             total_time: 0.0,
             n_flips: 0,
             n_heads_in_a_row: 0,
+            coin_val: 0.01,
+            multiplier: 1.0,
+            cash: 0.0,
         }
     }
 
@@ -51,6 +63,9 @@ impl Game {
             // Heads!
             // Update `n_heads_in_a_row`
             self.n_heads_in_a_row += 1;
+
+            // Update the amount of cash
+            self.cash += Game::calc_reward(self.coin_val, self.multiplier, self.n_heads_in_a_row);
         } else {
             // Tails
             // Reset `n_heads_in_a_row`
@@ -72,6 +87,15 @@ impl Game {
             self.flip();
         }
         None
+    }
+
+    /// A stateless function for calculating the reward given the current reward, the
+    /// current multiplier, and the number of heads in a row. For the first head in a
+    /// streak, the `current_reward` is the `coin_value`; after that, `current_reward`
+    /// is the reward we got last time.
+    #[allow(clippy::cast_possible_truncation, clippy::cast_possible_wrap)]
+    fn calc_reward(coin_value: f64, multiplier: f64, n_heads_in_a_row: usize) -> f64 {
+        multiplier.powi((n_heads_in_a_row as i32) - 1).ceil() * coin_value
     }
 }
 
@@ -102,6 +126,7 @@ fn save_usize_csv(data: &[usize]) -> std::io::Result<PathBuf> {
     Ok(path)
 }
 
+#[allow(clippy::cast_precision_loss)]
 fn main() {
     let max_iters = 2_000_000;
     let n_games = 1_000;
@@ -121,6 +146,9 @@ fn main() {
                 total_time: _,
                 n_flips,
                 n_heads_in_a_row: _,
+                coin_val: _,
+                multiplier: _,
+                cash: _,
             }) => {
                 results.push(n_flips);
             }
@@ -129,11 +157,55 @@ fn main() {
     }
 
     let run_time = start_time.elapsed();
+    #[allow(clippy::cast_possible_wrap)]
     let avg_run_time = run_time.div_f64(n_games as f64);
     println!("Ran in {run_time:?}, at about {avg_run_time:?} per game");
 
     match save_usize_csv(&results) {
         Ok(path) => println!("Saved the data to {}", path.display()),
         Err(_) => println!("Failed to save the data to file. Printing here.\n\n{results:?}"),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use approx::assert_relative_eq;
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(1.0, 3.0, 1, 1.0)]
+    #[case(1.0, 3.0, 2, 3.0)]
+    #[case(1.0, 3.0, 3, 9.0)]
+    #[case(1.0, 3.0, 4, 27.0)]
+    #[case(1.0, 3.0, 5, 81.0)]
+    #[case(0.1, 3.0, 1, 0.1)]
+    #[case(0.1, 3.0, 2, 0.3)]
+    #[case(0.1, 3.0, 3, 0.9)]
+    #[case(0.1, 3.0, 4, 2.7)]
+    #[case(0.1, 3.0, 5, 8.1)]
+    #[case(1.0, 2.5, 1, 1.0)]
+    #[case(1.0, 2.5, 2, 3.0)]
+    #[case(1.0, 2.5, 3, 7.0)]
+    #[case(1.0, 2.5, 4, 16.0)]
+    #[case(1.0, 2.5, 5, 40.0)]
+    #[case(0.25, 2.5, 1, 0.25)]
+    #[case(0.25, 2.5, 2, 0.75)]
+    #[case(0.25, 2.5, 3, 1.75)]
+    #[case(0.25, 2.5, 4, 4.0)]
+    #[case(0.25, 2.5, 5, 10.0)]
+    #[case(1.0, 1.0, 1, 1.0)]
+    #[case(1.0, 1.0, 2, 1.0)]
+    #[case(1.0, 1.0, 3, 1.0)]
+    #[case(1.0, 1.0, 4, 1.0)]
+    #[case(1.0, 1.0, 5, 1.0)]
+    fn test_combo_mult(
+        #[case] coin_value: f64,
+        #[case] multiplier: f64,
+        #[case] n_heads_in_a_row: usize,
+        #[case] expected: f64,
+    ) {
+        let got = Game::calc_reward(coin_value, multiplier, n_heads_in_a_row);
+        assert_relative_eq!(expected, got);
     }
 }
--- tests/game.rs
+++
@@ -1,8 +0,0 @@
-#[cfg(test)]
-mod tests {
-
-    #[test]
-    fn test_placeholder() {
-        assert_eq!(2, 2);
-    }
-}
